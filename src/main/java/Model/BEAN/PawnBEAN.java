56387a8fd8b23e3e3e83bdb6c4bb8dedf747ad8b
CongTien13 <congtien130105@gmail.com>
2025-09-27 12:57:24 +0700

    fix move pawn

--- src/main/java/Model/BEAN/PawnBEAN.java
+++ src/main/java/Model/BEAN/PawnBEAN.java
@@ -10,15 +10,21 @@ public class PawnBEAN extends PieceBEAN {
         int dx = newX - x;
         int dy = newY - y;
         if (color.equals("Red")) {
-            if (y <= 4) { 
+            // Red pawn is on its side of the river (y >= 5)
+            if (y >= 5) {
+                // Can only move forward
                 return dx == 0 && dy == -1;
-            } else {
+            } else { // Red pawn has crossed the river (y <= 4)
+                // Can move forward or sideways
                 return (dx == 0 && dy == -1) || (dy == 0 && Math.abs(dx) == 1);
             }
-        } else { 
-            if (y >= 5) {
+        } else { // Black pawn
+            // Black pawn is on its side of the river (y <= 4)
+            if (y <= 4) {
+                // Can only move forward
                 return dx == 0 && dy == 1;
-            } else {
+            } else { // Black pawn has crossed the river (y >= 5)
+                // Can move forward or sideways
                 return (dx == 0 && dy == 1) || (dy == 0 && Math.abs(dx) == 1);
             }
         }
